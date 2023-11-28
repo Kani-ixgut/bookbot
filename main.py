a512d596983fcfb1bebb6802a5aea156bb561c1a
kani-ixgut <kaniixgutcontact@gmail.com>
2023-11-28 10:13:12 +0100

    added MY WAY of counting letters in a given document (letters, meaning no numbers etc.)

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    words = count_words(text)
-    print(f"Words in frankenstein-book: {words}")
+    #words = count_words(text)
+    #print(f"Words in frankenstein-book: {words}")
+    letters = num_letters(text)
+    print(letters)
 
 
 def get_book_text(path):
@@ -16,5 +18,17 @@ def count_words(book):
     #    amount += 1
     #return amount
     return len(words)
+
+def num_letters(document):
+    letters = {}
+    alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    for letter in alphabet:
+        letters[letter] = 0
+    lowered = document.lower()
+    for letter in lowered:
+        if letter in alphabet:
+            letters[letter] += 1
+    return letters
+
 
 main()
